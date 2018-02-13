f44f680f9b977784570cf2dc82d8d538a6ff6ea0
arprasto <arprasto@in.ibm.com>
2018-02-13 12:58:31 +0530

    mobile control changes

--- IoT_MiniLab/PiStarter.py
+++ IoT_MiniLab/PiStarter.py
@@ -11,18 +11,22 @@ import json
 
 GPIO.setmode(GPIO.BOARD) # Use Physical Pin Numbering Scheme
 publish_led=22 # LED 1 is connected to physical pin 22
-response_led=18 # LED 2 is connected to physical pin 18
+cpu_temp_led=18 # LED 2 is connected to physical pin 18
+mobile_bulb=23
 iot_connection_led=12
 error_led=8
 GPIO.setup(publish_led,GPIO.OUT,) # Make LED 1 an Output
-GPIO.setup(response_led,GPIO.OUT) # Make LED 2 an Output
+GPIO.setup(cpu_temp_led,GPIO.OUT) # Make LED 2 an Output
+GPIO.setup(mobile_bulb,GPIO.OUT) # Make LED 2 an Output
 GPIO.setup(iot_connection_led,GPIO.OUT)
 GPIO.setup(error_led,GPIO.OUT)
 
 GPIO.output(publish_led,False)
-GPIO.output(response_led,False)
+GPIO.output(cpu_temp_led,False)
 GPIO.output(iot_connection_led,False)
 GPIO.output(error_led,False)
+GPIO.output(mobile_bulb,False)
+
 
 #read the IoT cred from file
 cred = json.loads(open("./sample_iot_cred.txt","r").read())
@@ -31,12 +35,16 @@ options = cred
 def commandProcessor(cmd):
  data=json.loads(json.dumps(cmd.data))
  print("Command received: %s" % json.dumps(cmd.data))
- if(data.get('power')=='ON'):
-  GPIO.output(response_led,True)
+ if(data.get('ledpower')=='ON'):
+  GPIO.output(cpu_temp_led,True)
   #sleep(7)
-  #GPIO.output(response_led,False)
+  #GPIO.output(cpu_temp_led,False)
+ elif(data.get('bulbpower')=='ON'):
+  GPIO.output(mobile_bulb,True)
+ elif(data.get('bulbpower')=='OFF'):
+  GPIO.output(mobile_bulb,False)
  else:
-  GPIO.output(response_led,False)
+  GPIO.output(cpu_temp_led,False)
   sleep(1)
 
 
@@ -69,4 +77,4 @@ while(1): # Create an infinite Loop
         print("Successfully published to IoT")
     GPIO.output(publish_led,False) # Turn LED off
     sleep(10)
-    GPIO.output(response_led,False)
+    GPIO.output(cpu_temp_led,False)
